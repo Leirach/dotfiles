e9288b999d7d3d6f2a099392a2c61eb1d4969ef4
Leirach <juandleon27@gmail.com>
2023-08-03 00:36:14 -0600

    Replaced ssh clone with https

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone git@github.com:Leirach/dotfiles.git
+git clone https://github.com/Leirach/dotfiles.git
 
 cp dotfiles/.zshrc ~/.zshrc
 cp dotfiles/custom.zsh-theme ~/.oh-my-zsh/custom/themes
