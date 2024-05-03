7577c62b1989504db700936595069dffcb41d78c
Leirach <juandleon27@gmail.com>
2024-05-03 02:19:57 -0600

    qol changes (if they work, untested)

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 export ZSH_BIN=$(which zsh)
 if [ -z "$ZSH_BIN" ]; then
-    sudo apt install zsh
+    sudo apt install -y zsh
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 cp .zshrc ~/.zshrc
 cp custom.zsh-theme ~/.oh-my-zsh/custom/themes
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
