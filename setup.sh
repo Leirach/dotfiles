2bc965b838279e7de4c4a6793ee302f5bac8e964
Juan De Leon <juan.deleon@lagersoft.com>
2026-01-15 15:31:22 -0600

    updated setup for nvim files

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-export ZSH_BIN=$(which zsh)
-if [ -z "$ZSH_BIN" ]; then
+if ! command -v zsh >/dev/null 2>&1; then
     sudo apt install -y zsh
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 cp .zshrc ~/.zshrc
-cp custom.zsh-theme ~/.oh-my-zsh/custom/themes
+cp custom.zsh-theme ~/.oh-my-zsh/custom/themes/custom.zsh-theme
+mkdir -p "$HOME/.config"
+cp -a .config/. "$HOME/.config/"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-chsh -s $(which zsh)
+chsh -s $(command -v zsh)
